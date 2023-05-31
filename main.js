dc427a04ce3f577a42ee0040e9e4ebc15722ac32
Giovanni Torrealba <47801129+g-torrealba@users.noreply.github.com>
2023-05-31 22:08:40 +0000

    Commit.2

--- main.js
+++ main.js
@@ -1,24 +1,119 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/** El cine
+ * Se conoce que el precio general de la entrada al cine es de $2
+ * Si es una persona de la tercera edad, entonces se otorga un 50% de descuento
+ * Para los estudiantes se les otorga un 40% de descuento si es miércoles (1=lunes)
+ * Se conoce de cada persona que llega al cine: su nombre, el tipo de persona
+ * (1=General, 2=Estudiante, 3=Tercera edad), para los estudiantes se conoce el día
+ * Se requiere: dado los datos de una persona, calcular el precio de su entrada.
+ * Para el cine, a. determinar la cantidad de personas que asistieron, b. el porcentaje de
+ * personas de la tercera edad, c. el monto total pagado por estudiantes y d. el monto
+ * total recaudado
+ * Usar herencia
+ * Ej:
+ * nombre tipo dia precioEntrada()
+ * Ana     2    2     $2.0
+ * Carla   1    -     $2.0
+ * Luis    3    -     $1.0
+ * Leo     2    3     $1.2
+ * R.a cantidadPersonasAsistieron()=4
+ * R.b porcentajePersonas3raEdad()=25%
+ * R.c montoTotalPagadoEstuds()=$3.2
+ * R.d montoTotalRecaudado()=$6.2
+ */
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+class Cl_mPersona {
+  constructor(nombre = "", tipo) {
+    this.nombre = nombre;
+    this.tipo = tipo;
+  }
 
-setupCounter(document.querySelector('#counter'))
+  precioEntrada() {
+    return this.entradaGral();
+  }
+
+  entradaGral() {
+    return 2;
+  }
+}
+
+class Cl_mAdultoMayor extends Cl_mPersona {
+  precioEntrada() {
+    return this.entradaGral() * 0.5;
+  }
+}
+
+class Cl_mEstudiante extends Cl_mPersona {
+  constructor(nombre, tipo, dia) {
+    super(nombre, tipo);
+    this.dia = dia;
+  }
+
+  precioEntrada() {
+    return this.entradaGral() * (this.dia === 3 ? 0.6 : 1);
+  }
+}
+
+class Cl_mCine {
+  constructor() {
+    this.cntPersonas =
+      this.cnt3raEdad =
+      this.acumMontoTotal =
+      this.acumMontoEstuds =
+        0;
+  }
+
+  procesarPersona(p) {
+    this.cntPersonas++;
+    if (p.tipo === 2) this.acumMontoEstuds += p.precioEntrada();
+    else if (p.tipo === 3) this.cnt3raEdad++;
+    this.acumMontoTotal += p.precioEntrada();
+  }
+
+  cantidadPersonasAsistieron() {
+    return this.cntPersonas;
+  }
+
+  porcentajePersonas3raEdad() {
+    return this.cntPersonas === 0
+      ? 0
+      : (this.cnt3raEdad / this.cntPersonas) * 100;
+  }
+
+  montoTotalPagadoEstuds() {
+    return this.acumMontoEstuds;
+  }
+
+  montoTotalRecaudado() {
+    return this.acumMontoTotal;
+  }
+}
+
+let cine = new Cl_mCine();
+
+let persona1 = new Cl_mEstudiante("Ana", 2, 2);
+cine.procesarPersona(persona1);
+let persona2 = new Cl_mPersona("Carla", 1);
+cine.procesarPersona(persona2);
+let persona3 = new Cl_mAdultoMayor("Luis", 3);
+cine.procesarPersona(persona3);
+let persona4 = new Cl_mEstudiante("Leo", 2, 3);
+cine.procesarPersona(persona4);
+
+let salida = document.getElementById("app");
+salida.innerHTML = "Resultados:";
+salida.innerHTML += "<br>Precio persona 1= " + persona1.precioEntrada();
+salida.innerHTML += "<br>Precio persona 2= " + persona2.precioEntrada();
+salida.innerHTML += "<br>Precio persona 3= " + persona3.precioEntrada();
+salida.innerHTML += "<br>Precio persona 4= " + persona4.precioEntrada();
+salida.innerHTML += "<br>";
+salida.innerHTML +=
+  "<br>Cantidad de personas que asistieron= " +
+  cine.cantidadPersonasAsistieron();
+salida.innerHTML += `<br>Porcentaje de personas de la 3ra edad=${cine.porcentajePersonas3raEdad()}%`;
+salida.innerHTML += `<br>Monto Total pagado por estudiantes=$${cine.montoTotalPagadoEstuds()}`;
+salida.innerHTML += `<br>Monto Total recaudado=$${cine.montoTotalRecaudado()}`;
+
+/*Obs:
+
+1) En el objeto persona1 hay que colocarle el tipo para que pueda mostrar el atributo correcto
+  y realizar los calculos respectivos*/
