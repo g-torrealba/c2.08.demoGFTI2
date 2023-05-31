7d8ca181e5cd83afafd7ee41dfbc43bcaac56a36
Giovanni <g_torrealba@ucla.edu.ve>
2023-05-31 18:14:36 -0400

    Cambio.2

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/** Precio de ropa
+ * Para calcular la ganancia que se asignará a la venta de una ropa, simplemente
+ * se incrementa el 20% del costo de la prenda
+ * Para el caso de ropa íntima se tiene: la ganancia es el 30% del costo si es de hombre
+ * y 40% si es de mujer
+ * Se tienen los siguientes datos de una ropa: nombre, costo, tipo(1=general, 2=íntima) y sexo ('M'-'F')
+ * Se requiere calcular el precio de venta de la ropa, usando herencia
+ */
+class Cl_mRopa {
+   constructor(nombre = "", costo = "") {
+      this.nombre = nombre;
+      this.costo = costo;
+   }
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+   precio() {
+      return this.costo + this.ganancia();
+   }
 
-setupCounter(document.querySelector('#counter'))
+   ganancia() {
+      return this.costo * 0.2;
+   }
+}
+
+class Cl_mIntimo extends Cl_mRopa {
+   constructor(nombre = "", costo = "", sexo = "") {
+      super(nombre, costo);
+      this.sexo = sexo;
+   }
+
+   ganancia() {
+      return this.costo * (this.sexo === "M" ? 0.3 : 0.4);
+   }
+}
+
+let prenda1 = new Cl_mRopa("Pantalón", 10);
+let prenda2 = new Cl_mIntimo("Brasier", 20, "F");
+
+let salida = document.getElementById("app");
+salida.innerHTML = "Resultados:";
+salida.innerHTML += "<br>Precio $$ 1= " + prenda1.precio();
+salida.innerHTML += "<br>Precio $$ 2= " + prenda2.precio();
